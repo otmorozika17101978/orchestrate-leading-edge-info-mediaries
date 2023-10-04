b6474aca4052f588552b0cd0b0d0096f9ffb2429
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-10-04 10:18:48 +0300

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+
+}
+    return data;
+    const data = generateRandomData();
+    });
+function generateRandomData() {
+    }
+}
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+
+main();
+        console.log("Random Number: " + item);
