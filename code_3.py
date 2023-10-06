a52b63fa1ba00ff61eb15192c59b884ee46e7ce9
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-10-06 17:47:00 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,26 +1,68 @@
+}
+    public static void main(String[] args) {
 
-def generate_random_data():
-    for item in data:
-    main()
+public class RandomDataGenerator {
+        for (int item : data) {
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
 
-    data = generate_random_data()
-
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():
-    return data
-import random
-        print(f"Random Number: {item}")        print(f"Random Number: {item}")
-def main():
-import random
-
-def generate_random_data():
-
+        for (int i = 0; i < 10; i++) {
+        }
+    }import random
     main()
     return data
+
+def generate_random_data():
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+
+def main():
     for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+}
+main();
+    data.forEach(item => {
+    return data;
+    });
+function main() {
+function generateRandomData() {
+
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+}
+    const data = [];
+
+    for (let i = 0; i < 10; i++) {
+    }
+        console.log("Random Number: " + item);
+
+        print(f"Random Number: {item}")
     data = generate_random_data()
 
-if __name__ == "__main__":
+    for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
+    main()
+import random
+
+if __name__ == "__main__":
+    return data
+def main():
+def generate_random_data():if __name__ == "__main__":
+    data = generate_random_data()
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+
+def main():
+
+
+    for item in data:
+    main()
+def generate_random_data():
+
+    return data
