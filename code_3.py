1f23864323f4957d22604f309c802fab0205504f
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-10-11 08:40:50 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,51 @@
+        }
+            System.out.println("Random Number: " + item);
 
-        print(f"Random Number: {item}")
-def generate_random_data():
+        int[] data = new int[10];
+        for (int item : data) {
 
-import random
-
-if __name__ == "__main__":
-    main()
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+        }
+    }if __name__ == "__main__":
 def main():
-    for item in data:
-
-    data = generate_random_data()
     data = [random.randint(1, 100) for _ in range(10)]
     return data
+    data = generate_random_data()
+import random
+    main()
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+
+    for item in data:
+
+    for item in data:
+        print(f"Random Number: {item}")
+import random
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
+    data = generate_random_data()
+
+def generate_random_data():
+
+
+    main()
+    return data        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+
+        }
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+
+}
